e54c45dede2627f5e02e3a3138f283a25e786d97
Yuri Furusho <yuricfurusho@gmail.com>
2017-11-14 01:17:46 -0200

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
+import * as path from 'path';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'about', component: AboutComponent}
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
